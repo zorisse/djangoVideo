50cc08bd6a08848f755e40b37fdb81e0d2f4acc3
Nouri T <zorisse@gmail.com>
2019-08-30 13:20:11 +0200

    database created

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Genre (models.Model):
+    #nom = models.typefield
+    name = models.CharField(max_length=255)
+
+
+class Movie (models.Model):
+    # name = models.type de champ
+    name = models.CharField(max_length=255)
+    date_sortie = models.IntegerField()
+    nbr_en_stock = models.IntegerField()
+    prix = models.FloatField()
+    # relation
+    # nom = models.ForeignKey(CLass, on_delete= models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    # time stamp
+    date_created = models.DateTimeField(default=timezone.now)
